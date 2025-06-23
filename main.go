c233f7b2f86b071d75c99e9b29edb2819e13cd82
woland on pc <ariana.ghasemzade.asl@gmail.com>
2025-06-23 22:24:09 +0330

    Add All Modules

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-// init project
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	//
+}
