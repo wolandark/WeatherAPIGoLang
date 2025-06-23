9fec2327f470dce857dec4ec196a673a1cc8d766
woland on pc <ariana.ghasemzade.asl@gmail.com>
2025-06-24 00:28:33 +0330

    Add DB Connection & Env Setup

--- main.go
+++ main.go
@@ -50,3 +50,48 @@ type WeatherAPIResponse struct {
 	} `json:"current"`
 }
 
+var db *gorm.DB
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	apiKey := os.Getenv("WEATHER_API_KEY")
+	if apiKey == "" {
+		log.Fatal("WEATHER_API_KEY environment variable is required")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	if dbUser == "" {
+		dbUser = "root"
+	}
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	if dbName == "" {
+		dbName = "weather_db"
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	db = database
+	db.AutoMigrate(&Weather{})
+
+	log.Println("Database connected and migrated successfully")
+}
